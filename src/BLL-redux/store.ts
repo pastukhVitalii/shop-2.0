5ac53838afb46af4698b99e3e75500c9913edd22
pastukhVitalii <pastukh.v.7@gmail.com>
2021-03-19 15:02:05 +0200

    page 404,  refactored router

---
+++ src/BLL-redux/store.ts
@@ -0,0 +1,26 @@
+import {combineReducers} from "redux";
+import {productsReducer} from "./productsReducer";
+import {configureStore} from "@reduxjs/toolkit";
+import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form'
+import {authReducer} from "../features/Login/auth-reducer";
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+  auth: authReducer,
+  form: formReducer
+})
+
+export type RootReducerType = typeof rootReducer
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunkMiddleware)
+})
+
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<RootReducerType>
+
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store
